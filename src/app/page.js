7418feae82a48ac50ee9e98914f107b620ea77e6
Jeremiah Franco <jeremiahfranco3@gmail.com>
2025-08-11 20:52:52 -0700

    Added project desc

--- src/app/page.js
+++ src/app/page.js
@@ -66,7 +66,7 @@ export default function Home() {
         </div>00
       </div>
 
-      <main className="relative z-10 mx-4 sm:mx-8 lg:mx-32 xl:mx-128">
+      <main className="relative z-10 mx-4 sm:mx-8 lg:mx-32 xl:mx-64">
 
         {/* INTRO SECTION with fade */}
         <div
@@ -109,30 +109,89 @@ export default function Home() {
         </div>
 
         {/* PROJECTS SECTION */}
-        <section id="project" className="h-screen flex flex-col justify-center">
+        <section id="project" className="min-h-screen flex flex-col justify-center">
           <h1 className="text-[50px] font-bold border-b border-b-white">Projects</h1>
-          <h2 className="mt-6 text-xl font-bold">REC Services</h2>
-          <p className="italic mb-4">
-            Brigham Young University - Idaho / Apr 2025 - August 2025
-          </p>
-          <ul className="list-disc pl-6">
-            <li>
-              Developed a comprehensive web application for managing service requests,
-              technician assignments, and customer communications.
-            </li>
-            <li>
-              Implemented real-time notifications and reporting features to enhance user experience
-              and operational efficiency.
-            </li>
-          </ul>
-          <h2 className="mt-6 text-xl font-bold">Movie Web Finder</h2>
-          <p className="italic mb-4">Brigham Young University - Idaho / Apr 2025 - August 2025</p>
-          <h2 className="mt-6 text-xl font-bold">Scholar Manager</h2>
-          <p className="italic mb-4">Brigham Young University - Idaho / Apr 2025 - August 2025</p>
-          <h2 className="mt-6 text-xl font-bold">Class Roulette</h2>
-          <p className="italic mb-4">Brigham Young University - Idaho / Apr 2025 - August 2025</p>
-          <h2 className="mt-6 text-xl font-bold">Network Design II</h2>
-          <p className="italic mb-4">Brigham Young University - Idaho / Apr 2025 - August 2025</p>
+            <h2 className="mt-6 text-xl font-bold">REC Services</h2>
+              <p className="italic mb-4">
+                Brigham Young University - Idaho / Apr 2025 - August 2025
+              </p>
+              <ul className="list-disc pl-6">
+                <li>
+                  Developed a comprehensive web application for managing service requests,
+                  technician assignments, and customer communications for REC Services department at Brigham
+                  Young University - Idaho
+                </li>
+                <li>
+                  Implemented real-time notifications and reporting features to enhance user experience
+                  and operational efficiency
+                </li>
+              </ul>
+            <h2 className="mt-6 text-xl font-bold">Movie Web Finder</h2>
+              <p className="italic mb-4">Brigham Young University - Idaho / Jan 2025 - Apr 2025</p>
+              <ul className="list-disc pl-6">
+                <li>
+                  Built a full-stack movie discovery app using Svelte and Supabase, tested by over 100 users
+                </li>
+                <li>
+                  Fetched movie data from an external API to enable search and display for 500,000+ movie titles
+                </li>
+                <li>
+                  Developed a user authentication system with Supabase, supporting secure login and session tracking for 100+
+                  registered users
+                </li>
+              </ul>
+            <h2 className="mt-6 text-xl font-bold">Scholar Manager</h2>
+              <p className="italic mb-4">Brigham Young University - Idaho / Sept 2024 - Dec 2024</p>
+              <ul className="list-disc pl-6">
+                <li>
+                  Designed and developed a full-stack web application using Node.js and MongoDB to help college students track and
+                  manage personal finances
+                </li>
+                <li>
+                  Implemented user authentication, data validation, and error handling to assure secure and accurate data
+                  management
+                </li>
+                <li>
+                  Integrated MongoDB to store and organize user data, facilitating real-time tracking of income and expenses for
+                  more than 500 college students
+                </li>
+              </ul>
+            <h2 className="mt-6 text-xl font-bold">Network Design II</h2>
+              <p className="italic mb-4">Brigham Young University - Idaho / Jan 2024 - Apr 2024</p>
+              <ul className="list-disc pl-6">
+                <li>
+                  Configured Cisco routers and switches with VLANs, DHCP, and OSPF areas to support branch network
+                  connectivity and segment traffic efficiently
+                </li>
+                <li>
+                  Installed and secured LAN and WAN infrastructure following best practices, including port security and
+                  basic network hardening techniques
+                </li>
+                <li>
+                  Maintained accurate documentation of device configurations using show run outputs for
+                  troubleshooting, audits, and backup purposes
+                </li>
+                <li>
+                  Applied networking concepts such as IPv4 addressing, NAT/PAT, and subnetting in real-world lab
+                  scenarios under supervision of a Senior Network Engineer
+                </li>
+              </ul>
+            <h2 className="mt-6 text-xl font-bold">Class Roulette</h2>
+            <p className="italic mb-4">Brigham Young University - Idaho / Sept 2023 - Dec 2023</p>
+            <ul className="list-disc pl-6">
+              <li>
+                Collaborated with a team of 6 to develop a web application helps teachers store class information and randomly
+                select students with a single click
+              </li>
+              <li>
+                Influenced development of front-end interface, enhancing user experience and making student selection process
+                quick and intuitive
+              </li>
+              <li>
+                Cooperated closely with team to ensure proper integration between front-end and data handling logic, resulting in
+                smooth and reliable application performance
+              </li>
+            </ul>
         </section>
 
         {/* CONTACT FORM SECTION */}
